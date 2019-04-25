996eed2423825738b6e6364667441ca16936ee07
Ferran Vila Conesa <fnva@gft.com>
2019-04-25 08:59:36 +0200

    feat(lit-element): add slots to webcomponent

--- packages/lit-element-playground/src/my-element.ts
+++ packages/lit-element-playground/src/my-element.ts
@@ -2,12 +2,13 @@ import { LitElement, html, customElement, property } from 'lit-element'
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-    @property()
-    foo = 'foo'
+    @property({ type: String }) foo = 'foo'
 
     render() {
         return html`
             <p>${this.foo}</p>
+            <slot name="one"></slot>
+            <slot name="two"></slot>
         `
     }
 }
